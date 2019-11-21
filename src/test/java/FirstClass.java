5abba7d680f44683597e700cbfa15bd97f6e87ac
nejmi <uyghur@nejmis-MacBook-Pro.local>
2019-11-21 16:31:59 -0500

    second master

---
+++ src/test/java/FirstClass.java
@@ -0,0 +1,17 @@
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("najmdin");
+        System.out.println("aksu");
+
+
+    }
+
+
+
+
+
+
+}
+
---
+++ src/test/java/SeconClass.java
@@ -0,0 +1,16 @@
+public class SeconClass {
+
+    public static void main(String[] args) {
+
+
+     String myname= "humeyra" ;
+        System.out.println(myname);
+
+
+    }
+
+
+
+
+
+}
